d9e91df6cd10658250eb0e73943df9408ff074ca
lijixue <lijixue@digisky.com>
2017-11-14 10:37:28 +0800

    init

---
+++ env.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+# Author : Li Jixue
+# Date : 2017/11/08
+# Description: 环境设置
+# ------------------------------------------------------------------------------
+server_info = {
+    '测试': {
+        'release_en': 'test',
+        'ip': '192.168.60.169',
+        'port': 22,
+        'fight_ip': '192.168.60.169',
+        'username': 'honor',
+        'password': 'xx'
+    },
+    '日文': {
+        'release_en': 'jp',
+        'ip': '210.73.210.83',
+        'port': 4399,
+        'fight_ip': '210.73.210.83',
+        'username': 'honor',
+        'password': 'zK2p@dM$bh'
+    },
+    '韩文': {
+        'release_en': 'kr',
+        'ip': '210.73.210.93',
+        'port': 4399,
+        'fight_ip': '210.73.210.94',
+        'username': 'honor',
+        'password': 'zK2p@dM$bh'
+    },
+    '台湾': {
+        'release_en': 'hk',
+        'ip': '210.73.210.93',
+        'port': 4399,
+        'fight_ip': '210.73.210.94',
+        'username': 'honor',
+        'password': 'zK2p@dM$bh'
+    },
+    '东南亚': {
+        'release_en': 'sea',
+        'ip': '210.73.210.93',
+        'port': 4399,
+        'fight_ip': '210.73.210.94',
+        'username': 'honor',
+        'password': 'zK2p@dM$bh'
+    }
+}
+
+
+root_path = '/home/honor/online/'
